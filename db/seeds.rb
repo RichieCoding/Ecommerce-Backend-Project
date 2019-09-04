fd49db4cfb6adf825f4295b62cd72290be473216
Richard Li <richardli125@gmail.com>
2019-09-04 10:03:55 -0400

    reseeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+pickleSmall = Product.create( name: "Pickle Rick T-Shirt", size: "Small", color: 'black', quantity: 50, price: 25, varientID: "Rick01", imageUrl: "https://hottopic.scene7.com/is/image/HotTopic/11025327_hi?$pdp_hero_zoom$", description: "A pickle will rise... 
+  This Season 3 teaser T-shirt from Adult Swim's Rick and Morty features a Pickle Rick's Kosher Pickles logo design on front. Grab this tee now and get ready to find out why Rick has pickled himself when the series returns this summer!")
+pickleMedium = Product.create( name: "Pickle Rick T-Shirt", size: "Medium", color: 'black', quantity: 50, price: 25, varientID: "Rick01", imageUrl: "https://hottopic.scene7.com/is/image/HotTopic/11025327_hi?$pdp_hero_zoom$", description: "A pickle will rise... 
+  This Season 3 teaser T-shirt from Adult Swim's Rick and Morty features a Pickle Rick's Kosher Pickles logo design on front. Grab this tee now and get ready to find out why Rick has pickled himself when the series returns this summer!")
+rickNeonSmall = Product.create( name: "Rick and Morty Neon Tropical T-Shirt", size: "Small", color: 'white', quantity: 50, price: 22, varientID: "Rick02", imageUrl: "https://hottopic.scene7.com/is/image/HotTopic/11938005_hi?$pdp_hero_zoom$", description: "Rick and Morty must have traveled through a tropical portal because this tee features Rick wearing a Hawaiian shirt and lei and holding a fruity drink on the front and Morty on the back.")
+rickNeonMedium = Product.create( name: "Rick and Morty Neon Tropical T-Shirt", size: "Medium", color: 'white', quantity: 50, price: 22, varientID: "Rick02", imageUrl: "https://hottopic.scene7.com/is/image/HotTopic/11938005_hi?$pdp_hero_zoom$", description: "Rick and Morty must have traveled through a tropical portal because this tee features Rick wearing a Hawaiian shirt and lei and holding a fruity drink on the front and Morty on the back.")
+
+
+
+
