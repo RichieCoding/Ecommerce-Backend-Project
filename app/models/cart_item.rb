7be84025f4720b56bf646f3f09c505d036b5443e
Richard Li <richardli125@gmail.com>
2019-09-10 22:26:53 -0400

    updated cart item

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,16 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  def products
+    # debugger
+    product = self.product_id
+    Product.find(product) 
+  end
+
+  # def cart_id
+  #   cart_id = self.cart_id
+  #   CartItem.find_by(cart_id: cart_id)
+  # end
+
 end
