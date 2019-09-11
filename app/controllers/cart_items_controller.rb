b995e8ffcad4d8977a0bf3aed1eb05e14ae406ba
Richard Li <richardli125@gmail.com>
2019-09-10 22:25:18 -0400

    added cart update function

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,34 @@
 class CartItemsController < ApplicationController
+
+  def index
+    cart_items = CartItem.all
+    render json: cart_items, methods: [:products]
+  end
+
+  def show
+    user=User.find(params[:id])
+    cart=user.cart 
+    cart_item=cart.cart_items
+    render json: cart_item, methods: [:products]
+  end
+
+  # def create
+  #   if current_user
+  #     cart_item = CartItem.create(product_params)
+  #     render json: cart_item
+  #   else
+  #     render json: { errors: product.errors.full_messages }
+  #   end
+  # end
+
+  def update 
+    cart_item = CartItem.find(params[:id])
+    # byebug
+    cart_item.count=params[:count]
+    cart_item.save
+    render json: cart_item
+  end
+
+  
+
 end
