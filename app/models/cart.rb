8675966b3d6171c93744a4b60dd3d77e24b855b7
Richard Li <richardli125@gmail.com>
2019-09-10 22:26:38 -0400

    updated cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,19 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items
+  has_many :products , through: :cart_items
 
   def products
     product = self.cart_items.map { |items| items.product_id }
-    Product.find(product)
+    current_product = Product.find(product)
+    # current_cart_item = CartItem.find_by({product_id: current_product.id})
+    current_product
+  end
+
+  def cart_products
+    # product = self.cart_items.map { |items| Product.find(items.id) }
+    # hello = product.map{ | item | CartItem.find(item.id) }
+    self.cart_items
   end
 
 end
