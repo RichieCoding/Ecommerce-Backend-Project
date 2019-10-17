e99950ec6524f6df59d810808b4c470549c03893
Richard Li <richardli125@gmail.com>
2019-10-17 17:07:15 -0400

    added username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :orders
   has_one :cart
 
+  validates :username, uniqueness: true
+
   def create_cart
     user = self.id 
     Cart.create(user_id: user)
