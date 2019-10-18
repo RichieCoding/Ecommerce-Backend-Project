7a1928ec21ec25f8e246db2d081c58e782fb823a
Richard Li <richardli125@gmail.com>
2019-10-18 17:26:07 -0400

    added phone_number to params and changed seed file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :email, :address, :city, :state, :zipcode, :username, :password)
+    params.permit(:first_name, :last_name, :phone_number, :email, :address, :city, :state, :zipcode, :username, :password)
   end
 end
