8694890953a42ce7a473b6e73a1bd7bd121748f4
Richard Li <richardli125@gmail.com>
2019-10-20 17:04:21 -0400

    added category to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_143817) do
+ActiveRecord::Schema.define(version: 2019_10_20_044411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_143817) do
     t.string "imageUrl"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
